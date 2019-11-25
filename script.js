86e1377709c090c45a5fdd1f38934c36ead59df9
codessive <jokurio6@gmail.com>
2019-11-25 18:23:53 +0100

    ft: Places search enabled

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 var map;
 var autocomplete;
 function geoMap() {
-    var mapProp= {
+    var mapDisplay= {
       center:new google.maps.LatLng(6.465422, 3.406448),
       zoom:5,
     };
-     map = new google.maps.Map(document.getElementById("geo-map"),mapProp);
+     map = new google.maps.Map(document.getElementById("geo-map"),mapDisplay);
      var input = document.getElementById("search");
-     var options = {
-       types: ['cities']
-     };
-     autocomplete = new google.map.places.Autocomplete(input, options);
-    }
+     var searchBox = new google.maps.places.SearchBox(input);
+
+     map.addListener('bounds_changed', function() {
+       searchBox.setBounds(map.getBounds());
+     });
+   
+     var markers = [];
+     
+     searchBox.addListener('places_changed', function () {
+       var places = searchBox.getPlaces();
+   
+       if (places.length == 0)
+         return;
+   
+       markers.forEach(function (m) { m.setMap(null); });
+       markers = [];
+   
+       var bounds = new google.maps.LatLngBounds();
+       places.forEach(function(p) {
+         if (!p.geometry)
+           return;
+   
+         markers.push(new google.maps.Marker({
+           map: map,
+           title: p.name,
+           position: p.geometry.location
+         }));
+   
+         if (p.geometry.viewport)
+           bounds.union(p.geometry.viewport);
+         else
+           bounds.extend(p.geometry.location);
+       });
+       
+       map.fitBounds(bounds);
+     });
+   }  
+   

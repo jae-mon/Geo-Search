d36b16ca429e1c436bc8e93b9f827dc709e75963
codessive <jokurio6@gmail.com>
2019-11-27 11:48:19 +0100

    ch: API Updates

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
-var map;
-var autocomplete;
+let map;
+let local;
+let bounds;
+let infoWindow;
+let currentWindow;
+let service;
+let infoPane;
+let autocomplete;
 function geoMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(6.465422, 3.406448),
-      zoom:5,
-    };
-     map = new google.maps.Map(document.getElementById("geo-map"),mapProp);
-     var input = document.getElementById("search");
-     var options = {
-       types: ['cities']
-     };
-     autocomplete = new google.map.places.Autocomplete(input, options);
-    }
+  bounds = new google.maps.LatLng();
+  infoWindow = new google.maps.InfoWindow;
+  currentWindow = infoWindow;
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      local = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map = new google.maps.Map(document.getElementById('geo-map'),{
+        center: local,
+        zoom: 10
+      });
+      bounds.extend(local);
+
+      infoWindow.setPosition(local);
+      infoWindow.setContent('City found.');
+      infoWindow.open(map);
+      map.setCenter(local);
+    }, () => {
+      //Browser supports geolocation, but user has denied permission
+      handleLocationError(true, infoWindow);
+    });
+  } else {
+    //Browser doesn't support geolocation
+    handleLocationError(false, infoWindow);
+  }
+}
+// Handle a geolocation error
+function handleLocationError(browserHasGeolocation, infoWindow) {
+  // Set default location to Lagos, Nigeria
+  local = {lat: 6.465422, lng: 3.406448};
+  map = new google.maps.Map(document.getElementById("geo-map"), {
+    center: local,
+    zoom: 10
+  });
+
+  infoWindow.setPosition(local);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Geolocation permission denied. Using default location.' :
+    'Error: Your browser doesn\'t support geolocation');
+    infoWindow.open(map);
+    currentWindow = infoWindow;
+}  
+   
